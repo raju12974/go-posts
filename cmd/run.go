73a33f0adcf7842b59de310b61acc3baf6c6a442
raju12974 <11677791+raju12974@users.noreply.github.com>
2025-01-22 10:44:22 +0000

    Post installation operations

--- cmd/run.go
+++ cmd/run.go
@@ -1,7 +1,7 @@
 package cmd
 
 import (
-	"github.com/ankorstore/yokai-http-template/internal"
+	"github.com/raju12974/go-posts/internal"
 	"github.com/spf13/cobra"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/ankorstore/yokai-http-template
+module github.com/raju12974/go-posts
 
 go 1.23
 
